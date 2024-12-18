a1405e421fd4e070d327dd029037b6427c990f99
pravar-anu <pravar.anu@arcadis.com>
2024-12-18 15:10:12 +0530

    Added new icon for iphone

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,50 @@ function App() {
 
   return (
     <div>
-      <h1>This one is for testing</h1>
+      <h1>This one is for testing purpose only</h1>
       <Helmet>
         <title>Nya wala Title</title>
         <link rel="icon" type='image/x-icon' href="/icons/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/icons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/icons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/icons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/icons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/icons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/apple-icon-152x152.png"
+        />
       </Helmet>
     </div>
   )
