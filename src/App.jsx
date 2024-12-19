210b632d5d6e762cd02e252e0204747ea506f212
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 14:35:27 +0530

    Take 9

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <link rel="icon" type="image/png" href="/favicon-96x96.png?v2" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg?v2" />
         <link rel="icon shortcut" href="/favicon.ico?v2" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v2" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v2" /> */}
         <meta name="apple-mobile-web-app-title" content="PAC" />
         <link rel="manifest" href="/site.webmanifest" />
         {/* <link rel="apple-touch-icon" href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/client_logo/PA Connect logo.svg" /> */}
