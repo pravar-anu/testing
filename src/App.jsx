1965836b9a835aeac452a6fbff9d06d35eb565fa
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 11:00:38 +0530

    Changes in icon

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,46 @@ function App() {
       <h1>This one is for testing purpose only</h1>
       <Helmet>
         <title>Nya wala Title</title>
-        <link rel="icon" type='image/x-icon' href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/favicon/paconnect_favicon.ico"/>
+        <link rel="icon" type='image/x-icon' href="%PUBLIC_URL%/icons/favicon.ico"/>
         <link
           rel="apple-touch-icon"
           sizes="57x57"
-          href="../public/icons/apple-icon-57x57.png"
+          href="%PUBLIC_URL%/icons/apple-icon-57x57.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
-          href="../public/icons/apple-icon-60x60.png"
+          href="%PUBLIC_URL%/icons/apple-icon-60x60.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="72x72"
-          href="../public/icons/apple-icon-72x72.png"
+          href="%PUBLIC_URL%/icons/apple-icon-72x72.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href="../public/icons/apple-icon-76x76.png"
+          href="%PUBLIC_URL%/icons/apple-icon-76x76.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="114x114"
-          href="../public/icons/apple-icon-114x114.png"
+          href="%PUBLIC_URL%/icons/apple-icon-114x114.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="120x120"
-          href="../public/icons/apple-icon-120x120.png"
+          href="%PUBLIC_URL%/icons/apple-icon-120x120.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="144x144"
-          href="../public/icons/apple-icon-144x144.png"
+          href="%PUBLIC_URL%/icons/apple-icon-144x144.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="../public/icons/apple-icon-152x152.png"
+          href="%PUBLIC_URL%/icons/apple-icon-152x152.png"
         />
       </Helmet>
     </div>
