b4aa412e80b708cd33e5e4883f11c6508eb02c31
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 15:23:31 +0530

    Take 17

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,9 @@ function App() {
       <h1>This one is for testing purpose only</h1>
       <Helmet>
         <title>Nya wala Title</title>
-        {/* <link rel="icon" href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/favicon/paconnect_favicon.ico" /> */}
-        <link rel="icon" type="image/png" href="/favicon-96x96.png?v2" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v2" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon shortcut" href="/favicon.ico?v2" />
-        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v2" /> */}
-        {/* <link rel="apple-touch-icon" sizes="16x16" href="/favicon-16x16.png?v2" /> */}
-        <meta name="apple-mobile-web-app-title" content="PAC" />
-        <link rel="manifest" href="/site.webmanifest" />
-
-{/* Testing Part */}
         <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
         <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
         <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
@@ -27,6 +20,8 @@ function App() {
         <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
         <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="manifest" href="/site.webmanifest" />        
+        <meta name="apple-mobile-web-app-title" content="PAC" />
       </Helmet>
     </div>
   )
