c41d81e916bc3c27d129160fda54917085cd96bd
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 14:26:54 +0530

    Take 8

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
       <Helmet>
         <title>Nya wala Title</title>
         {/* <link rel="icon" href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/favicon/paconnect_favicon.ico" /> */}
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png?v2" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg?v2" />
+        <link rel="icon shortcut" href="/favicon.ico?v2" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v2" />
+        <meta name="apple-mobile-web-app-title" content="PAC" />
         <link rel="manifest" href="/site.webmanifest" />
         {/* <link rel="apple-touch-icon" href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/client_logo/PA Connect logo.svg" /> */}
         {/* <link rel="icon" type='image/x-icon' href="%PUBLIC_URL%/icons/favicon.ico?v=2"/>
