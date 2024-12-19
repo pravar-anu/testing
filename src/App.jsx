3f7f6878f357f1108cdd24177685894b369eb8f4
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 14:47:52 +0530

    Take 13

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,9 @@ function App() {
       <Helmet>
         <title>Nya wala Title</title>
         {/* <link rel="icon" href="https://paconnect.dev.ibidigital.com/media/417cc314-603e-4bae-97c7-d39a775dc318/public/favicon/paconnect_favicon.ico" /> */}
-        <link rel="icon" type="image/png" href="/favicon-96x96.png?v2" sizes="96x96" />
+        {/* <link rel="icon" type="image/png" href="/favicon-96x96.png?v2" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg?v2" />
-        <link rel="icon shortcut" href="/favicon.ico?v2" />
+        <link rel="icon shortcut" href="/favicon.ico?v2" /> */}
         {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v2" /> */}
         {/* <meta name="apple-mobile-web-app-title" content="PAC" /> */}
         <link rel="manifest" href="/site.webmanifest" />
