e607987a36d461b188b775f49f8285eb2a66bfc0
pravar-anu <pravar.anu@arcadis.com>
2024-12-19 11:26:35 +0530

    Changes in new icon

--- src/App.jsx
+++ src/App.jsx
@@ -8,70 +8,70 @@ function App() {
       <h1>This one is for testing purpose only</h1>
       <Helmet>
         <title>Nya wala Title</title>
-        <link rel="icon" type='image/x-icon' href="%PUBLIC_URL%/icons/favicon.ico"/>
+        <link rel="icon" type='image/x-icon' href="%PUBLIC_URL%/icons/favicon.ico?v=2"/>
         <link
           rel="apple-touch-icon"
           sizes="57x57"
-          href="%PUBLIC_URL%/icons/apple-icon-57x57.png"
+          href="%PUBLIC_URL%/icons/apple-icon-57x57.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
-          href="%PUBLIC_URL%/icons/apple-icon-60x60.png"
+          href="%PUBLIC_URL%/icons/apple-icon-60x60.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="72x72"
-          href="%PUBLIC_URL%/icons/apple-icon-72x72.png"
+          href="%PUBLIC_URL%/icons/apple-icon-72x72.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href="%PUBLIC_URL%/icons/apple-icon-76x76.png"
+          href="%PUBLIC_URL%/icons/apple-icon-76x76.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="114x114"
-          href="%PUBLIC_URL%/icons/apple-icon-114x114.png"
+          href="%PUBLIC_URL%/icons/apple-icon-114x114.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="120x120"
-          href="%PUBLIC_URL%/icons/apple-icon-120x120.png"
+          href="%PUBLIC_URL%/icons/apple-icon-120x120.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="144x144"
-          href="%PUBLIC_URL%/icons/apple-icon-144x144.png"
+          href="%PUBLIC_URL%/icons/apple-icon-144x144.png?v=2"
         />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="%PUBLIC_URL%/icons/apple-icon-152x152.png"
+          href="%PUBLIC_URL%/icons/apple-icon-152x152.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="%PUBLIC_URL%/icons/android-icon-192x192.png"
+          href="%PUBLIC_URL%/icons/android-icon-192x192.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="%PUBLIC_URL%/icons/favicon-32x32.png"
+          href="%PUBLIC_URL%/icons/favicon-32x32.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="96x96"
-          href="%PUBLIC_URL%/icons/favicon-96x96.png"
+          href="%PUBLIC_URL%/icons/favicon-96x96.png?v=2"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="%PUBLIC_URL%/icons/favicon-16x16.png"
+          href="%PUBLIC_URL%/icons/favicon-16x16.png?v=2"
         />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
